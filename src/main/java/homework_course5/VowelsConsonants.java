15434daf3f72d75645b6c2fce149c4a3199f33fd
unknown <ramona_tigan@yahoo.ro>
2021-08-12 23:37:02 +0300

    [Course5]VowelsConsonants

--- src/main/java/homework_course5/VowelsConsonants.java
+++ src/main/java/homework_course5/VowelsConsonants.java
@@ -1,26 +1,21 @@
 package homework_course5;
 
+import java.util.Locale;
+
 public class VowelsConsonants {
     public static void main(String[] args) {
         int v = 0, c = 0;
         String str = "Follow your dreams, keep sleeping";
-        for (int x = 0; x < str.length(); ++x ){
+        str = str.toLowerCase();
+    for (int x = 0; x < str.length(); x++ ){
             char ch = str.charAt(x);
+
             switch (str.charAt(x)) {
                 case 'a':
-                    ch = 'a';
-
                 case 'e':
-                    ch = 'e';
-
                 case 'i':
-                    ch = 'i';
-
                 case 'o':
-                    ch = 'o';
-
                 case 'u':
-                    ch = 'u';
                     v++;
                     break;
 
